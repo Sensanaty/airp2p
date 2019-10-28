42253c7b92d78bd55210e6670c7304bb5a50f591
Andrea B <ab85@pm.me>
2019-10-28 18:20:36 +0800

    all routes created, controllers and respective actions generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :listings do
+    resources :rentals, only: [:create]
+  end
+
+  resources :rentals, only: [:show] do
+    resources :reviews, only: [:create, :edit, :update, :destroy]
+  end
+
+  resources :users, only: :show
+
+  get '/dashboard', to: 'dashboard#overview'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
