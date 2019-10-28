049a3a0f9d1d3b223c15dcef6624ddba3e0e3235
Josh Unwin <joshunwin@protonmail.com>
2019-10-28 17:43:01 +0800

    fixed reference to users table in rentals migration file

--- db/migrate/20191028081707_create_rentals.rb
+++ db/migrate/20191028081707_create_rentals.rb
@@ -2,7 +2,7 @@ class CreateRentals < ActiveRecord::Migration[5.2]
   def change
     create_table :rentals do |t|
       t.references :listing, foreign_key: true
-      t.references :customer_id, foreign_key: true 
+      t.references :customer_id, index: true, foreign_key: { to_table: :users }
       t.integer :rental_fee
       t.date :start_date
       t.date :end_date
