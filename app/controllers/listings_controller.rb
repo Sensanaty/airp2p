4bebb210b4378fdc1f573fa09d4886dfb437cf6c
jeroen rutten <rutten.jr@gmail.com>
2019-10-29 11:54:14 +0800

    Added most actions to linstings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,14 +1,24 @@
 class ListingsController < ApplicationController
+  before_action :set_garden, only: [:show, :edit, :update, :destroy]
   def show
+    @listing = Listing.find(params[:id])
   end
 
   def index
+    @listing = Listing.All
   end
 
   def new
+    @listing = Listing.new
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +29,14 @@ class ListingsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_garden
+      @listing = Listing.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def listing_params
+      params.require(:listing).permit(:price, :location, :game_id, :user_id)
+    end
 end
