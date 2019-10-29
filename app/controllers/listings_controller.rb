d07604bc14cb9fb2b9145c7ab1ea04a6bc659dd4
jeroen rutten <rutten.jr@gmail.com>
2019-10-29 12:39:30 +0800

    All the listing controller actions and the associated pages

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,10 @@
 class ListingsController < ApplicationController
-  before_action :set_garden, only: [:show, :edit, :update, :destroy]
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
   def show
-    @listing = Listing.find(params[:id])
   end
 
   def index
-    @listing = Listing.All
+    @listings = Listing.all
   end
 
   def new
@@ -25,18 +24,27 @@ class ListingsController < ApplicationController
   end
 
   def update
+    @listing.update(listing_params)
+    if @pet.save
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @listing.destroy
+    redirect_to dashboard_path
   end
 
   private
-    def set_garden
-      @listing = Listing.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def listing_params
-      params.require(:listing).permit(:price, :location, :game_id, :user_id)
-    end
+  def set_garden
+    @listing = Listing.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def listing_params
+    params.require(:listing).permit(:price, :location, :game_id, :user_id)
+  end
 end
