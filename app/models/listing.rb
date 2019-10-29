8442a4a8270b8779ae9c78293be01187361f8b1f
jeroen rutten <rutten.jr@gmail.com>
2019-10-29 17:55:45 +0800

    Added platform to the new listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,6 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :rentals
 
-  consoles = ["PS4", "Xbox One", "Nintendo Switch", "PC"]
-  validates :platform, inclusion: { in: consoles }
+  CONSOLES = ["PS4", "Xbox One", "Nintendo Switch", "PC"]
+  validates :platform, inclusion: { in: CONSOLES }
 end
