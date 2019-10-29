ae6b95cd987dceecac700fd014350592917cb74f
Sensanaty <sensanati@gmail.com>
2019-10-29 12:12:59 +0800

    Update seed with user generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 genres = JSON.parse(open("db/json/genres.json").read)
-
 read_games = open("db/json/games.json").read
 games = JSON.parse(read_games)
 
 puts "\nGenerating a Game table with 500 entries\e[5m...\n\e[25m"
+
 games.each do |game|
   genre_array = genres.find { |hash| hash["id"] == game["genres"].first }
   title = game["name"]
@@ -12,4 +12,26 @@ games.each do |game|
   Game.create(title: title, description: description, genre: genre_array["slug"], platform: platform)
 end
 
-puts "\n\e[42mSeed File finished running"
+puts "\nGames generated"
+
+# User generation
+require 'faker'
+
+puts "\nGenerating 50 random users"
+50.times do
+  email = Faker::Internet.unique.email
+  username = "#{Faker::Games::Fallout.character}#{Faker::Games::SuperSmashBros.fighter}".gsub(/\s+/, "").capitalize
+  bio = Faker::Lorem.sentence(word_count: 10)
+  avatar = Faker::Avatar.image
+  p "Email: #{email}"
+  p "Username: #{username}"
+  p "Bio: #{bio}"
+  p "Avatar Link: #{avatar}"
+  p "======================="
+  User.create(email: email, password: "password",
+              username: username, profile_info: bio, photo: avatar)
+end
+
+puts "\nUsers generated"
+
+puts "\n\e[42mSeed File finished running\e[49m.\n"
