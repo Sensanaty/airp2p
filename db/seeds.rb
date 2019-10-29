97954351de5ba13b286f476820aa8f5f3f8ee7d7
Luka <sensanati@gmail.com>
2019-10-29 15:01:18 +0800

    Update Seed with Game and User generation (#19)
    * Update seed with user generator
    
    * Remove unnecessary logging from seeds
    
    * Destroy tables on seed run
    
    * Remove useless JSON files
    
    * Seed with Fakerized games and Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,28 @@
-require 'json'
+require 'faker'
 
+# Reset DBs for Seed
 Game.destroy_all
 User.destroy_all
 
-genres = JSON.parse(open("db/json/genres.json").read)
-read_games = open("db/json/games.json").read
-games = JSON.parse(read_games)
+puts "NOTE: Due to Faker limitations we can only create 200 UNIQUE games"
+puts "\nGenerating a Game table with 200 entries\e[5m...\n\e[25m"
 
-puts "\nGenerating a Game table with 500 entries\e[5m...\n\e[25m"
-
-games.each do |game|
-  genre_array = genres.find { |hash| hash["id"] == game["genres"].first }
-  title = game["name"]
-  description = game["summary"]
-  platform = game["platforms"]
-
-  Game.create(title: title, description: description, genre: genre_array["slug"], platform: platform)
+200.times do
+  name = Faker::Game.unique.title
+  genre = Faker::Game.genre
+  platform = Faker::Game.platform
+  bio = Faker::Lorem.sentence(word_count: 15)
+  image = 'https://picsum.photos/150/190'
+  Game.create(
+          title: name,
+          description: bio,
+          genre: genre,
+          platform: platform,
+          image: image
+  )
 end
 
-puts "\nGames generated"
-
-# User generation
-require 'faker'
+puts "\nGames generated\n========"
 
 puts "\nGenerating 50 random users"
 50.times do
@@ -30,10 +31,13 @@ puts "\nGenerating 50 random users"
   bio = Faker::Lorem.sentence(word_count: 10)
   avatar = Faker::Avatar.image
 
-  User.create(email: email, password: "password",
-              username: username, profile_info: bio, photo: avatar)
+  User.create(
+      email: email,
+      password: "password",
+      username: username,
+      profile_info: bio,
+      photo: avatar)
 end
 
 puts "\nUsers generated"
-
 puts "\n\e[42mSeed File finished running\e[49m.\n"
