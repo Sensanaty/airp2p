b86a2cfb6e14b757874723c297b8fe37df1ac63a
Josh Unwin <joshunwin@protonmail.com>
2019-10-29 17:18:12 +0800

    Work on dashboard controller view (#28)
    * added dashboard link to navbar, then built out basic version of dashboard overview
    
    * fixed reference to rentals in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :listings
-  has_many :rentals
+  has_many :rentals, foreign_key: "customer_id", class_name: "Rental"
+
   has_many :reviews, through: :rentals
 end
