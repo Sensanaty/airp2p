e9305fc6251f19dc44aaa236f2c16031b25a223d
Sensanaty <sensanati@gmail.com>
2019-10-29 11:42:26 +0800

    Finish part of Games seeding TODO Parse consoles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,17 @@
 require 'json'
 
-genres = JSON.parse(open("json/genres.json").read)
-genres.each_with_index do |_, index|
-  genre_hash = {
-      id: index,
-      name: genres[index]["slug"]
-  }
-  genre_hash[:name]
-end
+genres = JSON.parse(open("db/json/genres.json").read)
 
 read_games = open("db/json/games.json").read
 games = JSON.parse(read_games)
-p games[0]["name"]
-p games[0]["summary"]
-p games[0]["platforms"]
-puts "\n\n\n"
-games.each_with_index do |_, index|
-  puts "Line #{index + 1}"
-  p "Genre:"
-  p "Title: #{games[index]["name"]}"
-  p "Description: #{games[index]["summary"]}"
-  p "Genre: #{}"
-  p "Platform: #{games[index]["platforms"]}"
-  puts "=========="
-  Game.create(title: games[index]["name"], description: games[index]["summary"], genre: genres[index]["name"], platform: games[index]["platforms"])
+
+puts "\nGenerating a Game table with 500 entries\e[5m...\n\e[25m"
+games.each do |game|
+  genre_array = genres.find { |hash| hash["id"] == game["genres"].first }
+  title = game["name"]
+  description = game["summary"]
+  platform = game["platforms"]
+  Game.create(title: title, description: description, genre: genre_array["slug"], platform: platform)
 end
 
-puts "\n\n============="
-puts "!!!!!!!!!!!!!"
-puts "!!!SUCCESS!!!"
-puts "!!!!!!!!!!!!!"
-puts "============="
+puts "\n\e[42mSeed File finished running"
