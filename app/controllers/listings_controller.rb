9da10d2bd464e7fd5b883f2c1a1c8e04a8fbe5c3
Josh Unwin <joshunwin@protonmail.com>
2019-10-30 11:14:20 +0800

    tweaked homepage looks, added links to the games cards on homepage

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,7 +4,14 @@ class ListingsController < ApplicationController
   end
 
   def index
-    @listings = Listing.all
+    if params[:search]
+      @search_query = params[:search][:name]
+      raise
+      @listings = Listing.joins(:games).where('title ILIKE ?', '%' + @search_query + '%' )
+      @title = "Results for \"#{@search_query}\""
+    else
+      @listings = Listing.all
+    end
   end
 
   def new
