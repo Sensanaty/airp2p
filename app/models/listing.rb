6570a0dfd47b504b9656588a0bd449bf4b5f1fc8
Josh Unwin <joshunwin@protonmail.com>
2019-10-30 11:42:05 +0800

    added search by game title to homepage using pg-search gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,17 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :rentals
 
+  include PgSearch::Model
+
   CONSOLES = ["PS4", "Xbox One", "Nintendo Switch", "PC"]
   validates :platform, inclusion: { in: CONSOLES }
+
+  pg_search_scope :global_search,
+    against: [ :platform ],
+    associated_against: {
+      game: [ :title ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
