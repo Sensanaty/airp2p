35dde2ad1716919ac6cdf4ee51bddf5f20b05e1d
Sensanaty <sensanati@gmail.com>
2019-10-30 12:10:00 +0800

    Add migration to add coordinates fields to listings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_092543) do
+ActiveRecord::Schema.define(version: 2019_10_30_040604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_092543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "platform"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["game_id"], name: "index_listings_on_game_id"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
