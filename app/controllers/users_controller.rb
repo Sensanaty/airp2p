4678c2ceede2fe4d14f6ebc71ca6fa9984e1d52c
Andrea B <ab85@pm.me>
2019-10-31 17:21:40 +0800

    Mostly Harmless_TM changes to usersshow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
     # authentication  and data display purposes
     @user_profile = User.find(params[:id])
     @user = current_user
+    @listings_id = @user_profile.listings.pluck(:id)
+    @rentals_id = Rental.where(listing_id: @listing_id).pluck(:id)
+    @reviews = Review.where(rental_id: @rentals_id)
     authorize @user
   end
 end
