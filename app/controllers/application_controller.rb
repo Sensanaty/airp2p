8ca57123d0fd98ec75a8b27e2f2c751346e7f217
Josh Unwin <joshunwin@protonmail.com>
2019-10-31 10:24:43 +0800

    updated new and edit user profile, along with navbar and dashboard with relevant links. Required adding lines to application controller (need to get profile photo upload working)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,9 @@
 class ApplicationController < ActionController::Base
 
-before_action :authenticate_user!
+before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   after_action :verify_authorized, except: [:index, :overview], unless: :skip_pundit?
-
-
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-      def configure_permitted_parameters
-          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :photo, :email, :password) }
-          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :photo, :email, :password, :current_password) }
-      end
-
 
 private
 
@@ -22,4 +11,10 @@ private
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/  || params[:controller] == 'rentals' || params[:controller] == 'review' || params[:controller] == 'users'
   end
 
+    protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :photo])
+  end
 end
