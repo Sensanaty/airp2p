ead2603f9801913c1e0cfe8eba755b4e80593771
Sensanaty <sensanati@gmail.com>
2019-11-01 12:00:13 +0800

    Maybe fix heroku flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import "flatpickr/dist/flatpickr.css";
+import "flatpickr/dist/themes/dark.css";
 
 
 import { initMapbox } from '../plugins/init_mapbox';
