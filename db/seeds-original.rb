658a82f57061a258c4783c7caf0261bbc1ea07bf
Josh Unwin <joshunwin@protonmail.com>
2020-05-30 01:28:33 +0200

    Fixed & improved seed file to get real games (#92)
    * created new seed file to create more realistic/accurate seeds. Edited footer and added quick favicon
    
    * removed dotenv import from seed file
    
    * improved note comment on env in seeds file
    
    * removed some commented out code
    
    Co-authored-by: Josh Unwin <joshunwin@gmail.com>

---
+++ db/seeds-original.rb
@@ -0,0 +1,93 @@
+require 'faker'
+require 'thegamesdb'
+
+# Reset DBs for Seed
+Game.destroy_all
+User.destroy_all
+Listing.destroy_all
+
+puts "NOTE: Due to Faker limitations we can only create 200 UNIQUE games"
+puts "\nGenerating a Game table with 200 entries\e[5m...\n\e[25m"
+
+200.times do
+  name = Faker::Game.unique.title
+  gamedb = Gamesdb.games_by_name(name)
+  genre = Faker::Game.genre
+
+  bio = if gamedb == []
+          Faker::Lorem.sentence(word_count: 15)
+        else
+          gamedb[0][:overview]
+        end
+
+  image = if gamedb == []
+            "https://source.unsplash.com/#{rand(500..600)}x#{rand(490..590)}/?games,gaming"
+          else
+            gamedb[0][:image]
+          end
+
+  Game.create(
+          title: name,
+          description: bio,
+          genre: genre,
+          image: image
+  )
+end
+
+puts "\nGames generated\n========"
+
+puts "\nGenerating 50 random users"
+50.times do
+  email = Faker::Internet.unique.email
+  username = "#{Faker::Games::Fallout.character}#{Faker::Games::SuperSmashBros.fighter}".gsub(/\s+/, "").capitalize
+  bio = Faker::Lorem.sentence(word_count: 10)
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create(
+      email: email,
+      password: "password",
+      username: username,
+      profile_info: bio,
+      first_name: first_name,
+      last_name: last_name
+  )
+end
+
+puts "\nUsers generated\n========"
+
+LOCATIONS = [
+    "Astaro Theatro, Sint Jansstraat, Burgwallen-Oude Zijde, Amsterdam, North Holland, The Netherlands",
+    "Dam, Dam Square, Centrum, Amsterdam, North Holland, The Netherlands",
+    "Albert Heijn, 34, Haarlemmerplein, Haarlemmerbuurt, Amsterdam, North Holland, The Netherlands",
+    "139-1, Overtoom, Overtoomse Sluis, Amsterdam, North Holland, Netherlands, 1054HG, The Netherlands",
+    "Camping Vliegenbos, 138, Meeuwenlaan, Amsterdam, North Holland, Netherlands, 1022AM, Netherlands",
+    "Vrije Universiteit, Campusplein, Zuidas, Amsterdam, North Holland, Netherlands, The Netherlands",
+    "Skek, 4, Zeedijk, Burgwallen-Oude Zijde, Amsterdam, North Holland, Netherlands, 1012AX, The Netherlands",
+    "EYE Filminstituut, 1, IJpromenade, Centrum, Amsterdam, North Holland, Netherlands, 1031KT, Netherlands",
+    "Entree de Borcht, Amstelpark, Buitenveldert, Amsterdam, North Holland, Netherlands, 1083AC, The Netherlands",
+    "Aquamarin, Weesp, North Holland, Netherlands, 1382KJ, Netherlands",
+    "Groot kinderbad, Grote Speelweide, Amstelveen, North Holland, Netherlands, 1182AD, The Netherlands",
+    "4, Koningin Julianalaan, Zanderij, Katwijk, South Holland, Netherlands, 2224EW, Netherlands",
+    "Dorpskerk, Gasthuislaan, 's- Gravenzande, 's-Gravenzande, Westland, South Holland, Netherlands",
+    "637, Simplonbaan, Hoograven, Utrecht, Netherlands, 3524GK, The Netherlands",
+    "City centre, Langstraat, UMCG, Centrum, Groningen, Netherlands, 9713GZ, The Netherlands",
+    "WTC (H), Zuidplein, Zuidas, Amsterdam, North Holland, Netherlands, 1077 XV, The Netherlands"
+]
+PLATFORMS = ["PS4", "Xbox One", "Nintendo Switch", "PC"]
+
+LOCATIONS.each_with_index do |_, index|
+  game_id = Game.find(rand(1..200)).id
+  user_id = User.find(rand(1..50)).id
+  price = rand(2..5)
+  Listing.create(
+             game_id: game_id,
+             user_id: user_id,
+             price: price,
+             location: LOCATIONS[index],
+             platform: PLATFORMS.sample
+  )
+end
+
+puts "\nListings created"
+puts "\n\e[42mSeed File finished running\e[49m.\n"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,62 +1,129 @@
 require 'faker'
-require 'thegamesdb'
+require 'rest-client'
+require 'pp'
 
-# Reset DBs for Seed
-Game.destroy_all
-User.destroy_all
-Listing.destroy_all
+# If you want to run this seed file directly (ie, via 'ruby ./seeds.rb') then you need the below two lines. 
+# Running the seed file in Rails will work fine without them (ie, 'rails db:seed'). These MUST be commented out for Heroku.
+# require 'dotenv'
+# Dotenv.load
 
-puts "NOTE: Due to Faker limitations we can only create 200 UNIQUE games"
-puts "\nGenerating a Game table with 200 entries\e[5m...\n\e[25m"
+def get_game_genres
+  puts "\e[34mGetting game genres...\e[0m"
+  url = "https://api.thegamesdb.net/v1/Genres?apikey=#{ENV['GAMESDB_API_KEY']}"
+  response = RestClient.get(url)
+  data = JSON.parse(response.body)
+  genres = data["data"]["genres"]
 
-200.times do
-  name = Faker::Game.unique.title
-  gamedb = Gamesdb.games_by_name(name)
-  genre = Faker::Game.genre
+  usable_genres = {}
 
-  bio = if gamedb == []
-          Faker::Lorem.sentence(word_count: 15)
-        else
-          gamedb[0][:overview]
-        end
+  # Convert to friendly hash for use in this seeding app
+  genres.each do |genre|
+    usable_genres[genre[1]["id"]] = genre[1]["name"]
+  end
 
-  image = if gamedb == []
-            "https://source.unsplash.com/#{rand(500..600)}x#{rand(490..590)}/?games,gaming"
-          else
-            gamedb[0][:image]
-          end
-
-  Game.create(
-          title: name,
-          description: bio,
-          genre: genre,
-          image: image
-  )
+  puts "\e[92mGot Genres!\e[0m\n\n"
+  return usable_genres
 end
 
-puts "\nGames generated\n========"
+$genres = get_game_genres
 
-puts "\nGenerating 50 random users"
-50.times do
-  email = Faker::Internet.unique.email
-  username = "#{Faker::Games::Fallout.character}#{Faker::Games::SuperSmashBros.fighter}".gsub(/\s+/, "").capitalize
-  bio = Faker::Lorem.sentence(word_count: 10)
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
+def main
+  platform_ids = [["1", "PC"], ["4919", "PS4"], ["4920", "Xbox One"], ["4971", "Nintendo Switch"]] # num = API platform ID
+  generate_users()
 
-  User.create(
-      email: email,
-      password: "password",
-      username: username,
-      profile_info: bio,
-      first_name: first_name,
-      last_name: last_name
-  )
+  platform_ids.each do |platform|
+    puts "\e[33mGetting games for #{platform[1]}...\e[0m"
+    games = get_games(platform[0])
+
+    if (platform[0] != "1")
+      pp "games: #{games}"
+    end
+
+    games.each do |game|
+      add_game_to_db(game)
+    end
+    puts "\e[92mFinished adding #{platform} games to database.\e[0m\n\n"
+    generate_listings(platform[1])
+  end
+
+  puts "\n\e[32m+=+=+=+=+=+=+=+=+=+=+=+=+=+\e[0m"
+  puts "\e[32m|| \e[42;97mSEEDING DATABASE DONE\e[0m \e[32m||"
+  puts "\e[32m+=+=+=+=+=+=+=+=+=+=+=+=+=+\e[0m\n"
 end
 
-puts "\nUsers generated\n========"
 
-LOCATIONS = [
+def get_games(platform_id)
+  url = "https://api.thegamesdb.net/v1/Games/ByPlatformID?apikey=#{ENV['GAMESDB_API_KEY']}&id=#{platform_id}&fields=overview%2Cgenres&page=3"
+  response = RestClient.get(url)
+  data = JSON.parse(response.body)
+  games = data["data"]["games"]
+  puts "\e[92mGot games!\e[0m\n\n"
+  return games
+end
+
+def add_game_to_db(game)
+  # pp game
+  puts "\e[34mBuilding #{game["game_title"]} object for database...\e[0m"
+
+  game_for_db = {
+    title: game["game_title"],
+    description: game["overview"],
+    genre: get_game_genre(game["genres"]),
+    image: get_game_image(game["id"])
+  }
+  puts "\e[34mAdding #{game["game_title"]} to database...\e[0m"
+  Game.create(game_for_db)
+  puts "\e[92mAdded #{game["game_title"]} to database!\e[0m\n\n"
+end
+
+def get_game_genre(genre_ids_array)
+  primary_genre = "Unknown"
+  primary_genre_id = 0
+
+  if genre_ids_array.kind_of?(Array)
+    primary_genre_id = genre_ids_array[0]
+  end
+  
+  if primary_genre_id != 0
+    primary_genre = $genres[primary_genre_id]
+  end
+
+  return primary_genre
+end
+
+def get_game_image(game_id)
+  api_url = "https://api.thegamesdb.net/v1/Games/Images?apikey=#{ENV['GAMESDB_API_KEY']}&games_id=#{game_id}"
+  response = RestClient.get(api_url)
+  data = JSON.parse(response.body)
+  image_metadata_array = data["data"]["images"][game_id.to_s]
+  boxart_front_metadata = image_metadata_array.select{|img| img["side"] == "front"}[0]
+  boxart_front_filename = boxart_front_metadata["filename"]
+  image_url = "https://cdn.thegamesdb.net/images/medium/#{boxart_front_filename}"
+end
+
+def generate_users
+  puts "\e[34mGenerating 50 random users...\e[0m"
+  50.times do
+    email = Faker::Internet.unique.email
+    username = "#{Faker::Games::Fallout.character}#{Faker::Games::SuperSmashBros.fighter}".gsub(/\s+/, "").capitalize
+    bio = Faker::Lorem.sentence(word_count: 10)
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+
+    User.create(
+        email: email,
+        password: "password",
+        username: username,
+        profile_info: bio,
+        first_name: first_name,
+        last_name: last_name
+    )
+  end
+  puts "\e[92m50 random users added to database!\e[0m\n\n"
+end
+
+def generate_listings(platform_name)
+  locations = [
     "Astaro Theatro, Sint Jansstraat, Burgwallen-Oude Zijde, Amsterdam, North Holland, The Netherlands",
     "Dam, Dam Square, Centrum, Amsterdam, North Holland, The Netherlands",
     "Albert Heijn, 34, Haarlemmerplein, Haarlemmerbuurt, Amsterdam, North Holland, The Netherlands",
@@ -73,23 +140,28 @@ LOCATIONS = [
     "637, Simplonbaan, Hoograven, Utrecht, Netherlands, 3524GK, The Netherlands",
     "City centre, Langstraat, UMCG, Centrum, Groningen, Netherlands, 9713GZ, The Netherlands",
     "WTC (H), Zuidplein, Zuidas, Amsterdam, North Holland, Netherlands, 1077 XV, The Netherlands"
-]
-PLATFORMS = ["PS4", "Xbox One", "Nintendo Switch", "PC"]
+  ]
+  
+  puts "\nGenerating #{locations.count} random listings with random users and random locations"
+  
+  locations.each_with_index do |_, index|
+    recently_added_games = Game.last(20)
+    game_id = recently_added_games.sample["id"]
+    user_id = User.find(rand(1..50)).id
+    price = rand(2..5)
 
-puts "\nGenerating #{LOCATIONS.count} random listings with random users and random locations"
+    listing_object = {
+      game_id: game_id,
+      user_id: user_id,
+      price: price,
+      location: locations[index],
+      platform: platform_name      
+    }
 
-LOCATIONS.each_with_index do |_, index|
-  game_id = Game.find(rand(1..200)).id
-  user_id = User.find(rand(1..50)).id
-  price = rand(2..5)
-  Listing.create(
-             game_id: game_id,
-             user_id: user_id,
-             price: price,
-             location: LOCATIONS[index],
-             platform: PLATFORMS.sample
-  )
+    Listing.create(listing_object)
+  end
+
+  puts "\e[92mFinished generating random listings!\e[0m\n\n"
 end
 
-puts "\nListings created"
-puts "\n\e[42mSeed File finished running\e[49m.\n"
+main
